20052a46a1ef55b7fb33649c1c391f0ac3adeeaa
pramodkumar0 <pramodpusala@gmail.com>
2023-01-18 17:33:50 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+// import Hollywood from './Components/Hollywood';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Bollywood from './Pages/Bollywood';
+import Technology from './Pages/Technology';
+import Hollywood from './Pages/Hollywood';
+import Fitness from './Pages/Fitness';
+import Food from './Pages/Food';
+function App() {
+  
+  return (
+     <>
+     <BrowserRouter>
+         
+         <Routes>
+             <Route path="/" element={<Home/>}/>
+             <Route path="/bollywood" element={<Bollywood/>}/>
+             <Route path="/technology" element={<Technology/>}/>
+             <Route path="/hollywood" element={<Hollywood/>}/>
+             <Route path="/fitness" element={<Fitness/>}/>
+             <Route path="/food" element={<Food/>}/>
+             
+         </Routes>
+         
+     </BrowserRouter>
+
+     {/* <Hollywood/> */}
+     </>
+  );
+}
+
+export default App;
